8b16b295265b76501437c81f8fedc9010331ee7d
Joel Neto <joelcsneto90@gmail.com>
2021-10-01 22:32:35 +0100

    improvements

---
+++ web-app/src/app/models/app/config.ts
@@ -0,0 +1,8 @@
+import { appModelBase } from "./appModelBase";
+
+export class Configuration {
+pageItems: number;
+user: string;
+password: string;
+url: string;
+}
--- web-app/src/environments/environment.prod.ts
+++ web-app/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true
   ,encryptSecret: "encryptSecret"
+  ,pageItems: 8
 };
